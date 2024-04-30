ad7b0f5366600052fc8be591e70f2809fe50e1dd
Hai Ho Dac <hai.hodac@pepperstone.com>
2024-04-30 11:58:15 +1000

    allow select model from ollama

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
+import ollama
 import streamlit as st
-
 from langchain_community.chat_models import ChatOllama
 from langchain_community.callbacks import StreamlitCallbackHandler
 
-model = "llama3"
 
+@st.cache_resource
+def list_model():
+    models = [m["name"].split(":")[0] for m in ollama.list()["models"]]
+    return models
+
+
+model = st.selectbox("Select model", list_model())
 st.header(f"Streamlit + Ollama + {model.capitalize()}")
 
 st.markdown("""---""")
